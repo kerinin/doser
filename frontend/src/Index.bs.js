adfafab95638c07e5c8dfda6145b1009323b3db6
Ryan Michael <kerinin@gmail.com>
2020-09-12 14:38:06 -0400

    Remove example code

--- frontend/src/Index.bs.js
+++ frontend/src/Index.bs.js
@@ -1,43 +1,9 @@
 'use strict';
 
 var React = require("react");
-var ReactDom = require("react-dom");
-var ExampleStyles$Doser = require("./ExampleStyles.bs.js");
-var BlinkingGreeting$Doser = require("./BlinkingGreeting/BlinkingGreeting.bs.js");
-var FetchedDogPictures$Doser = require("./FetchedDogPictures/FetchedDogPictures.bs.js");
-var ReducerFromReactJSDocs$Doser = require("./ReducerFromReactJSDocs/ReducerFromReactJSDocs.bs.js");
-var ReasonUsingJSUsingReason$Doser = require("./ReasonUsingJSUsingReason/ReasonUsingJSUsingReason.bs.js");
+var App$Doser = require("./App.bs.js");
+var ReactDOMRe = require("reason-react/src/legacy/ReactDOMRe.bs.js");
 
-var style = document.createElement("style");
+ReactDOMRe.renderToElementWithId(React.createElement(App$Doser.make, {}), "root");
 
-document.head.appendChild(style);
-
-style.innerHTML = ExampleStyles$Doser.style;
-
-function makeContainer(text) {
-  var container = document.createElement("div");
-  container.className = "container";
-  var title = document.createElement("div");
-  title.className = "containerTitle";
-  title.innerText = text;
-  var content = document.createElement("div");
-  content.className = "containerContent";
-  container.appendChild(title);
-  container.appendChild(content);
-  document.body.appendChild(container);
-  return content;
-}
-
-ReactDom.render(React.createElement(BlinkingGreeting$Doser.make, {
-          children: "Hello!"
-        }), makeContainer("Blinking Greeting"));
-
-ReactDom.render(React.createElement(ReducerFromReactJSDocs$Doser.make, {}), makeContainer("Reducer From ReactJS Docs"));
-
-ReactDom.render(React.createElement(FetchedDogPictures$Doser.make, {}), makeContainer("Fetched Dog Pictures"));
-
-ReactDom.render(React.createElement(ReasonUsingJSUsingReason$Doser.make, {}), makeContainer("Reason Using JS Using Reason"));
-
-exports.style = style;
-exports.makeContainer = makeContainer;
-/* style Not a pure module */
+/*  Not a pure module */
