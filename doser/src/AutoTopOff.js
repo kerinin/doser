aeee5a8bb66089977f6a19171bffd7018600d338
Ryan Michael <kerinin@gmail.com>
2020-09-13 21:41:21 -0400

    improve graph a bit

--- doser/src/AutoTopOff.js
+++ doser/src/AutoTopOff.js
@@ -4,7 +4,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import { VictoryBar } from 'victory';
+import { VictoryArea, VictoryAxis, VictoryBar, VictoryChart, VictoryLine, VictoryTheme } from 'victory';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -12,6 +12,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TablePagination from '@material-ui/core/TablePagination';
+import { useTheme } from '@material-ui/core/styles';
 
 import DoserAppBar from './DoserAppBar';
 import EditAutoTopOff from './EditAutoTopOff';
@@ -21,11 +22,36 @@ const useStyles = makeStyles((theme) => ({
     grid: {
         flexGrow: 1,
     },
+    line: {
+        stroke: theme.palette.primary.main,
+    },
 }));
 
 function AutoTopOff() {
+    const theme = useTheme();
     const classes = useStyles();
     let { atoId } = useParams();
+
+    const victoryTheme = {
+        ...VictoryTheme.material,
+        chart: {
+            ...VictoryTheme.material.chart,
+            width: 800,
+            height: 400,
+        },
+
+        line: {
+            ...VictoryTheme.material.line,
+            style: {
+                ...VictoryTheme.material.line.style,
+                data: {
+                    ...VictoryTheme.material.line.style.data,
+                    stroke: theme.palette.primary.main,
+                    strokeWidth: 4,
+                },
+            },
+        },
+    }
 
     const handleClick = () => {
         console.info('You clicked the Chip.');
@@ -49,7 +75,42 @@ function AutoTopOff() {
                         <Card>
                             <CardHeader title="History" />
                             <CardContent>
-                                <VictoryBar />
+                                <VictoryChart
+                                    theme={victoryTheme}
+                                >
+                                    <VictoryArea
+                                        interpolation="stepAfter"
+                                        minDomain={{ y: 0 }}
+                                        data={
+                                            [
+                                                { x: 1, y: 19000 },
+                                                { x: 2, y: 0 },
+                                                { x: 3, y: 16500 },
+                                                { x: 4, y: 0 },
+                                            ]
+                                        }
+                                        style={{
+                                            data: {
+                                                fill: theme.palette.secondary.main,
+                                            }
+                                        }}
+                                    />
+                                    <VictoryLine
+                                        interpolation="natural"
+                                        minDomain={{ y: 0 }}
+                                        data={
+                                            [
+                                                { x: 1, y: 13000 },
+                                                { x: 2, y: 16500 },
+                                                { x: 3, y: 14250 },
+                                                { x: 4, y: 19000 }
+                                            ]
+                                        }
+
+                                    />
+                                    <VictoryAxis></VictoryAxis>
+                                    <VictoryAxis dependentAxis></VictoryAxis>
+                                </VictoryChart>
                             </CardContent>
 
                             <CardContent>
